a21b2a8328fbbc3dd71811b4910df8faa47dff9c
Keith K <k.kellerman@dnp.imgcomm.com>
2020-05-03 16:19:40 -0400

    Added random method on Tower.cs

--- Tower.cs
+++ Tower.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TowerDefense 
 {
   class Tower
@@ -6,12 +8,22 @@ namespace TowerDefense
     
     private const int _range = 1;
     private const int _power = 1; 
+    private const double _accuracy = .75;
+    
+    private static readonly Random _random = new Random();                    //only tower class will use it
+    
+    
     
     private readonly MapLocation _location;
     
     public Tower(MapLocation location)
     {
        _location = location;
+    }
+    
+    public bool IsSuccessfulShot()
+    {
+        return _random.NextDouble() < _accuracy;      
     }
     
     public void FireOnInvaders(Invader[] invaders)
@@ -29,7 +41,25 @@ namespace TowerDefense
         
         if (invader.IsActive && _location.InRangeOf(invader.Location, _range))
         {
-         invader.DecreaseHealth(_power); 
+          
+          if(IsSuccessfulShot())
+          {
+              invader.DecreaseHealth(_power); 
+              Console.WriteLine("Shot at and hit an invader!");
+            
+            if(invader.IsNeutralized)
+            {
+             Console.WriteLine("Neutralized an invader!");  
+              
+            }
+          }
+          else
+          {
+           Console.WriteLine("Shot at and missed an invader.");
+            
+          }
+          
+          
           break;
           
         }
